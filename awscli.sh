765b003d2ab5bb93262bc311f7807b610d3e0b90
Vilas khandagale <vilaskhandagale1597@gmail.com>
2025-12-21 21:58:59 +0530

    Add eksctl installation to awscli.sh
    Added installation steps for eksctl to the script.

--- awscli.sh
+++ awscli.sh
@@ -5,7 +5,12 @@ unzip awscliv2.zip
 sudo ./aws/install
 aws --version
 
-#for macos
+#for mac os
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 aws --version
+# install eks
+curl -sLO https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz
+tar -xzf eksctl_$(uname -s)_amd64.tar.gz
+sudo mv eksctl /usr/local/bin
+
